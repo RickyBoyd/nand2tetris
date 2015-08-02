f977f6b89a35747da9edc4c9914394c32e11f728
Ricky Boyd <ricky_boyd@hotmail.co.uk>
2015-08-02 19:16:16 +0100

    Minor fix to a comment in assembler.

--- 06/assembler.py
+++ 06/assembler.py
@@ -117,7 +117,7 @@ def main():
 	content = [x for x in content if not x==""]
 	#print content
 
-	#remember what line each label was on and numbers that symbols represent
+	#Dictionary with predefined labels that can have new symbols added to it
 	labels = {
 			"R0":"0", "R1":"1", "R2":"2", "R3":"3", "R4":"4", "R5":"5", "R6":"6", "R7":"7", "R8":"8",
 			"R9":"9", "R10":"10", "R11":"11", "R12":"12", "R13":"13", "R14":"14", "R15":"15", "SP":"0",
