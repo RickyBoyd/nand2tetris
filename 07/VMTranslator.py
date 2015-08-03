36efc5b4dcf0e776b1606dbabd121fead537eaa7
Ricky Boyd <ricky_boyd@hotmail.co.uk>
2015-08-03 15:18:38 +0100

    Added file IO handling for VM translator.

--- 07/VMTranslator.py
+++ 07/VMTranslator.py
@@ -1,16 +1,43 @@
-import string, system, os
+import string, sys, os
+
+def newFileName(file_name):
+	return file_name.replace(".vm", "") + ".asm"
 
 def main():
 	file_name = sys.argv[1]
-	if file_name.endswith(".vm"):
-		new_file_name = file_name.remove(".vm", "") + ".asm"
-		print new_file_name
+	if file_name.endswith(".vm"): #Dealing with a single .vm file
+		new_file_name = newFileName(file_name)
 		for path, dirs, files in os.walk('.'):
-		if file_name in files:
-			os.chdir(path)
-			with open(file_name) as f:
-				content = f.readlines()
-			f.close()
-			#assembly = translate(content)
-			break
-	
+			if file_name in files:
+				os.chdir(path)
+				with open(file_name) as f:
+					content = f.readlines()
+				f.close()
+				print content
+				#TO BE IMPLEMENTED
+				#assembly = translate(content)
+				#f = open(new_file_name, "w")
+				#for line in assembly:
+				#	f.write(line)
+				#f.close()
+				break
+	elif not '.' in file_name: #Dealing with a directory
+		for path, dirs, files in os.walk("./"+file_name):
+			for f in files:
+				if f.endswith(".vm"):
+					os.chdir(path)
+					content = []
+					with open(f) as file_content:
+						content = file_content.readlines()
+
+					file_content.close
+					print content
+					#assembly = translate(content)
+					new_file_name = newFileName(f)
+					#f = open(new_file_name, "a")
+					#for line in assembly:
+					#	f.write(line)
+					#f.close()
+
+
+main()
