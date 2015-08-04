c6e183a80262a7e2195a3f4323d7f5ef8eb02232
Ricky Boyd <ricky_boyd@hotmail.co.uk>
2015-08-04 16:08:46 +0100

    Commands in VMTranslator now parsed into their compenents. Beginnning to write push and pop.

--- 07/VMTranslator.py
+++ 07/VMTranslator.py
@@ -1,10 +1,43 @@
 import string, sys, os
 
+def parse(content):
+	for i in range(len(content)):
+		content[i] = content[i].replace('\t', '')
+		content[i] = content[i].replace('\n', '')
+		content[i] = content[i].replace('\r', '')
+		content[i] = content[i].split("//", 1)[0]
+
+	#remove empty lines
+	content = [x for x in content if not x==""]
+	for i in range(len(content)):
+		content[i] = content[i].split(" ")
+	return content
+
+def pop(args):
+	#pop whatever is at the top of the stack to D
+	pop_asm = "@SP\nM=M-1\nA=M\nD=M\n"
+
+def push(args):
+	#push whatever is in D
+	push_asm = "@SP\nM=M+1\nA=M\nM=D\n"
+
+def constant(i):
+	number = str(i)
+	constant_asm = "@"+number+"\nD=A\n"
+
+def add_routine():
+	add_asm = "@SP\nM=M-1\nA=M\nD=M\nA=A-1\nM=M+D\n"
+	return
+
 def newFileName(file_name):
 	return file_name.replace(".vm", "") + ".asm"
 
+def initialise():
+	initialise_asm = "@SP\nM=256"
+
 def main():
 	file_name = sys.argv[1]
+	add_routine()
 	if file_name.endswith(".vm"): #Dealing with a single .vm file
 		new_file_name = newFileName(file_name)
 		for path, dirs, files in os.walk('.'):
@@ -13,7 +46,8 @@ def main():
 				with open(file_name) as f:
 					content = f.readlines()
 				f.close()
-				print content
+				print parse(content)
+				#print content
 				#TO BE IMPLEMENTED
 				#assembly = translate(content)
 				#f = open(new_file_name, "w")
@@ -31,7 +65,7 @@ def main():
 						content = file_content.readlines()
 
 					file_content.close
-					print content
+					#print content
 					#assembly = translate(content)
 					new_file_name = newFileName(f)
 					#f = open(new_file_name, "a")
